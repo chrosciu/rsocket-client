5df1841726b0ae93f40c442d1289320ca441d176
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-09-29 23:45:40 +0200

    duplex channel

---
+++ src/main/java/pl/com/sages/rsocketclient/NumberChat.java
@@ -0,0 +1,30 @@
+package pl.com.sages.rsocketclient;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class NumberChat implements CommandLineRunner {
+    private final Mono<RSocketRequester> rSocketRequester;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Flux<Integer> outbound = Flux.interval(Duration.ofSeconds(8)).map(l -> (int)(long)l);
+        Flux<Integer> inbound = rSocketRequester.flatMapMany(requester -> requester
+                .route("chat.number")
+                .data(outbound)
+                .retrieveFlux(Integer.class)
+        );
+        inbound.subscribe(i -> log.info("inbound: " + i), e -> log.warn("", e));
+
+    }
+}
