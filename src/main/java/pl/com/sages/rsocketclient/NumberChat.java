a83a29ed271357ba3f069318ff4f7dda375c5dd6
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-10-05 16:57:54 +0200

    duplex channel - conditional on property

--- src/main/java/pl/com/sages/rsocketclient/NumberChat.java
+++ src/main/java/pl/com/sages/rsocketclient/NumberChat.java
@@ -3,6 +3,7 @@ package pl.com.sages.rsocketclient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -13,11 +14,12 @@ import java.time.Duration;
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@ConditionalOnProperty(value="chat.enabled", havingValue = "true")
 public class NumberChat implements CommandLineRunner {
     private final Mono<RSocketRequester> rSocketRequester;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         Flux<Integer> outbound = Flux.interval(Duration.ofSeconds(8)).map(l -> (int)(long)l);
         Flux<Integer> inbound = rSocketRequester.flatMapMany(requester -> requester
                 .route("chat.number")
